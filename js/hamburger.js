81569e27d6315440335d65b0cd1aded3633454a6
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-05-01 10:40:50 +0900

    ハンバーガーメニューの機能部だけ
    タイトル通り、ハンバーガーメニューの機能だけ実装しました。まだページ遷移等の機能は追加していないこと、ご了承ください。

---
+++ js/hamburger.js
@@ -0,0 +1,64 @@
+//変数定義
+var navigationOpenFlag = false;
+var navButtonFlag = true;
+var focusFlag = false;
+ 
+//ハンバーガーメニュー
+    $(function(){
+      // ハンバーガーメニューボタンがクリックされた時の処理
+      $(document).on('click','.el_humburger',function(){
+        if(navButtonFlag){
+          spNavInOut.switch();
+          //連続してボタンを押せないようにする
+          setTimeout(function(){
+            navButtonFlag = true;
+          },200);
+          navButtonFlag = false;
+        }
+      });
+      // 画面のどこかをクリックした時の処理.今たぶん要素押してもメニュー閉じるだけになってる！
+      $(document).on('click touchend', function(event) {
+        if (!$(event.target).closest('.navi,.el_humburger').length && $('body').hasClass('js_humburgerOpen') && focusFlag) {
+          focusFlag = false;
+          //scrollBlocker(false);
+          spNavInOut.switch();
+        }
+      });
+    });
+ 
+//ナビ開く処理
+function spNavIn(){
+  $('body').removeClass('js_humburgerClose');
+  $('body').addClass('js_humburgerOpen');
+  setTimeout(function(){
+    focusFlag = true;
+  },200);
+  setTimeout(function(){
+    navigationOpenFlag = true;
+  },200);
+}
+ 
+//ナビ閉じる処理
+function spNavOut(){
+  $('body').removeClass('js_humburgerOpen');
+  $('body').addClass('js_humburgerClose');
+  setTimeout(function(){
+    $(".uq_spNavi").removeClass("js_appear");
+    focusFlag = false;
+  },200);
+  navigationOpenFlag = false;
+}
+ 
+//ナビ開閉コントロール
+var spNavInOut = {
+  switch:function(){
+    if($('body.spNavFreez').length){
+      return false;
+    }
+    if($('body').hasClass('js_humburgerOpen')){
+     spNavOut();
+    } else {
+     spNavIn();
+    }
+  }
+};
