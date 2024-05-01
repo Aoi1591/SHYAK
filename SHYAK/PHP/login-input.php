c56f4b102f5657d840568a1a106ba0cc3fa77132
koshi <2201035@s.asojuku.ac.jp>
2024-05-01 20:12:08 +0900

    ログイン画面(未完成)

---
+++ SHYAK/PHP/login-input.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>ログイン画面</title>
+    </head>
+    <body>
+        <form action = "login" method="post">
+            <input type="text" id="username" name="username" placeholder="USERNAME" required><br>
+
+            <input type="password" id="password" name="password" placeholder="PASSWORD" required><br>
+
+            <input type="submit" value="LOGIN">
+
+            <br><br>
+
+        <button onclick="window.location.href='signup-input.php';">SIGN-UP</button>
+        </form>
+    </body>
+</html>
