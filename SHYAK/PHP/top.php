0d0fdab1aa830fe23a83d1bfefb79ce0a4a61ed1
shion <2201376@s.asojuku.ac.jp>
2024-05-07 09:45:32 +0900

    a
    top

---
+++ SHYAK/PHP/top.php
@@ -0,0 +1,34 @@
+<div class="container text-center">
+    <div class="row justify-content-end">
+        <div class="col-12 col-md-6">
+            <div class="row">
+            <a href="settei.php">
+              <button type="submit" class="btn btn-outline-dark userinfoButton bg-light"><br>設定
+              </button>
+            </a>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container text-center">
+        <a href="MorauHenji.php">
+              <button type="submit" class="btn btn-outline-dark userinfoButton bg-light"><br>本
+              </button>
+            </a>
+        </div>
+        
+        <div class="row">
+            <a href="BinNagasu.php">
+              <button type="submit" class="btn btn-outline-dark userinfoButton bg-light"><br>瓶を流す
+              </button>
+            </a>
+        </div>
+
+        <div class="row">
+            <a href="BinKaisyuu.php">
+              <button type="submit" class="btn btn-outline-dark userinfoButton bg-light"><br>瓶を回収
+              </button>
+            </a>
+        </div>
+</div>
