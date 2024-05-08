d6823195e51f2d7c1058dc7927be5b35fe5b45f0
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-05-08 10:49:15 +0900

    軽い変更

--- SHYAK/PHP/header.php
+++ SHYAK/PHP/header.php
@@ -4,4 +4,3 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-    <title>Ocean Bottle</title>
