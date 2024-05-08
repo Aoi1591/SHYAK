08d86a68a775e1b8ca217f1b3ce1c13b21e7f2a6
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-05-08 10:06:09 +0900

    ハンバーガーメニューのアイコンについて
    各行コメントアウトでマークの説明を追加しました。

--- SHYAK/PHP/menu-hamburger.php
+++ SHYAK/PHP/menu-hamburger.php
@@ -8,10 +8,10 @@
     echo '</div>';
     echo '<header class="navi"><!--ナビゲーション-->';
     echo '<div class="navi_inner">';
-    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-anchor" style="color: #ffffff;"></i></a></div>';
-    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-user-group" style="color: #aed6f4;"></i></a></div>';
-    echo '<div class="navi_item"><a href=""><i class="fa-regular fa-envelope" style="color: #5291ff;"></i></a></div>';
-    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-arrow-right-from-bracket" style="color: #0b5fef;"></i></a></div>';
+    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-anchor" style="color: #ffffff;"></i></a></div>';//碇のマーク。マイページへ
+    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-user-group" style="color: #aed6f4;"></i></a></div>';//人のマーク。フレンド
+    echo '<div class="navi_item"><a href=""><i class="fa-regular fa-envelope" style="color: #5291ff;"></i></a></div>';//封筒のマーク。ルール説明
+    echo '<div class="navi_item"><a href=""><i class="fa-solid fa-arrow-right-from-bracket" style="color: #0b5fef;"></i></a></div>';//ログアウトのマーク。ログアウト
     echo '</div>';
     echo '</header>';
     echo '<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>';
