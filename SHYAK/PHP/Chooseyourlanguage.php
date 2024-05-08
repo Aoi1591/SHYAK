c5ac04bc7d0a34162fb22523b32e17ea370548b2
Hiroshi <137484523+Hiroshi7700@users.noreply.github.com>
2024-05-08 10:26:21 +0900

    Update Chooseyourlanguage.php

--- SHYAK/PHP/Chooseyourlanguage.php
+++ SHYAK/PHP/Chooseyourlanguage.php
@@ -1,11 +1,16 @@
 <?php
-    
+    require 'header.php';
+    echo '</head>';
     echo '<p><h1>Choose your language</h1></p>';
     echo '<input type="submit" value="戻る">';
     echo '<input type="submit" value="Français">';
     echo '<input type="submit" value="Русский">';
     echo '<input type="submit" value="中文">';
-    echo '<input type="submit" value="日本語">';
+    echo'<button a href="Chooseyourlanguage1.php" >日本語</a>';
+
+    echo '<button type="submit" class="btn btn-outline-dark userinfoButton bg-light"><br>本
+    </button>';
+
     echo '<input type="submit" value="English">';
     echo '<input type="submit" value="Português">';
 
