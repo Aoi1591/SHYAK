5e18c591f0a79b8b65d5d7c0052c7e9de4048b6b
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-05-08 11:54:27 +0900

    Update header.php

--- SHYAK/PHP/header.php
+++ SHYAK/PHP/header.php
@@ -1,6 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+<?php
+    echo '<!DOCTYPE html>';
+    echo '<html lang="ja">';
+    echo '<head>';
+    echo '<meta charset="UTF-8">';
+    echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+    echo '<link rel="stylesheet" href="hum.css">';
+    echo '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">';
+?>
