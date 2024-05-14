a370b16a36db896da6e46385a84c53829ea1cc5c
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-05-14 10:28:41 +0900

    管理者ログインのためのチェック
    特定の画像をダブルクリックしたときに管理者かどうかチェックして、管理者と判定されたら管理者用ログインページへ遷移するコードを作成しました。

--- SHYAK/PHP/AdminLogin-check.php
+++ SHYAK/PHP/AdminLogin-check.php
@@ -1,7 +1,7 @@
 <?php
     $secretDate = date('Ymd');
-    if ($_POST['secret'] === $secretDate) {
-        header("Location: ./AdminLogin-input.php");
+    if ($_POST['userInput'] === $secretDate) {
+        header("Location: ./adminlogin_input.php");
         exit;
     } else {//管理者認証できなかった時の処理
         echo '<script>alert("error");</script>';
--- SHYAK/PHP/login-input.php
+++ SHYAK/PHP/login-input.php
@@ -16,5 +16,30 @@
 
         <button onclick="window.location.href='signup-input.php';">SIGN-UP</button>
         </form>
+        <img src="#" class="button" ondblclick="check()" id="promptBtn"><!--srcには飛行機とかの画像を入れてね-->
+        <script>
+        document.getElementById("promptBtn").addEventListener("dbclick", function() {
+            // ユーザーに管理者ログインページへ遷移するためのパス入力を求める
+            const userInput = prompt("Secret Pass"); 
+            // ユーザーがパスを入力したときの処理
+            if(userInput !== null){
+                // フォームを作成
+                const form = docment.createElement("form");
+                form.method = "POST";
+                form.action = "AdminLogin-check.php";// 入力値を指定のページへ飛ばす
+
+                // 入力値送信用のテキストフィールド(非表示)を追加
+                const inputField =document.createElement("input");
+                inputField.type = "hidden";// 非表示指定
+                inputField.name = "userInput";// 遷移先でデータを受け取るための名前指定
+                inputField.value = userInput;// promptで取得した値を代入
+                form.appendChild(inputField);// このテキストフィールドをフォームに追加
+
+                // フォームをドキュメントに追加
+                document.body.appendChild(form); // フォームをドキュメントのボディに追加
+                form.submit();// 送信！
+            }
+        });
+        </script>
     </body>
 </html>
