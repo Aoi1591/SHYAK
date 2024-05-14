58fcd3c0755d5877e2d182563aab7a463b1ad2ca
koshi <2201035@s.asojuku.ac.jp>
2024-05-14 10:30:17 +0900

    input 仮完成

--- SHYAK/PHP/login-input.php
+++ SHYAK/PHP/login-input.php
@@ -3,18 +3,46 @@
     <head>
         <meta charset="utf-8">
         <title>ログイン画面</title>
+        <link rel="stylesheet" href="../css/login-input.css">
     </head>
     <body>
-        <form action = "login" method="post">
-            <input type="text" id="username" name="username" placeholder="USERNAME" required><br>
+        <div id = "container">
+            <div id="background">
+                <div class="bottol-1"></div>
+                <div class="bottol-2"></div>
+                <div class="bottol-3"></div>
+                <div class="bottol-4"></div>
+                <div class="bottol-5"></div>
+                <div class="airplane"></div>
+            </div>
+            <form action = "login-output.php" method="post">
 
-            <input type="password" id="password" name="password" placeholder="PASSWORD" required><br>
+                <div id = "username-all">
+                    <div class="username">
+                    <input type="text" id="username" name="username" placeholder="USERNAME" required><br>
+                    </div>
+                </div>
 
-            <input type="submit" value="LOGIN">
+                <div id = "password-all">
+                    <div class = "password">
+                    <input type="password" id="password" name="password" placeholder="PASSWORD" required><br>
+                    </div>
+                </div>
 
-            <br><br>
+                <div id ="login-all"> 
+                    <div class="login"> 
+                    <input type="submit" value="LOGIN">
+                    </div>
+                </div>
 
-        <button onclick="window.location.href='signup-input.php';">SIGN-UP</button>
-        </form>
+                <br><br>
+
+            <div id="sign-up">
+                <div class="sign-up-line">
+                <a href = "../php/sign-up-input.php">sign-up</a>
+                </div>
+            </div>
+            </form>
+        </div>
     </body>
 </html>
---
+++ SHYAK/signup-input.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>サインアップ</title>
+    </head>
+    <body>
+    <script src="../JavaScript/signup-input.js"></script>
+         <form action ="signup-output.php" method="post">
+            <select name="choice" value="Language Choice">
+                <option value="Language Choice">Language Choice</option>
+                <option value = "Japanease">日本語</option>
+                <option value = "English">English</option>
+                <option value = "Russian"> русский язык</option>
+                <option value = "Portuguese">português</option>
+                <option value = "French">français</option>
+                <option value = "Chinese">中文</option>
+            </select>    
+            <input type="text" id="username" name="username" placeholder="USERNAME" required><br>
+            <input type="password" id="password" name="password" placeholder="PASSWORD" required><br>
+            <input type="password" id="re-enter-password" name="re-enter-password" placeholder="RE-ENTER-PASSWORD" required><br>
+            <input type="submit" value="Register">
+        </form>
+        <button class="back-button" onclick="goBack()">🔙</button>
+    </body>
+</html>
