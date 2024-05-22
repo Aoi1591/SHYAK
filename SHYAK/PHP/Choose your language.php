2cb28e9a28ea5c1428da5d545c6286f01f5ffdf3
Hiroshi <137484523+Hiroshi7700@users.noreply.github.com>
2024-05-22 12:39:22 +0900

    a

--- SHYAK/PHP/Choose your language.php
+++ SHYAK/PHP/Choose your language.php
@@ -1,19 +1,19 @@
-<?php
-    require 'header.php';
-    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require 'header.php'; ?>
     <title>Choose your language</title>
+    <link rel="stylesheet" href="../css/Chooseyourlanguage.css">
 </head>
 <body>
-<?php
-    
-    echo '<div style="text-align: center">';
-    echo '<h1>Choose your language</h1>';
-    echo '<h1>この瓶でよろしいですか？</h1>';
-    echo '<input type="submit" value="はい">';
-    echo '<a href="Chooseyourlanguage.php">いいえ</a>';
-    echo '</div>';
-
-?>
+    <div id="header">
+        <h1>Choose your language</h1>
+    </div>
+    <div id="content">
+        <h1>この瓶でよろしいですか？</h1>
+        <input type="submit" value="はい">
+        <a href="Chooseyourlanguage.php">いいえ</a>
+    </div>
 </body>
 </html>
    
