85a176a758d93a6e25847c976535c1111b13739f
shion <2201376@s.asojuku.ac.jp>
2024-05-22 11:35:37 +0900

    a

--- SHYAK/PHP/Binnagasu-input.php
+++ SHYAK/PHP/Binnagasu-input.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,20 +12,24 @@
     <link rel="stylesheet" href="../CSS/Binnagasu.css">
 </head>
 <body>
+    <br>
     <div class="container">
         <!-- ✘ボタン -->
         <div class="row justify-content-end">
             <div class="col-12 col-md-2">
                 <div class="row">
-                    <br>
+                    
                     <a href="top.php">
-                        <button type="button" class="btn btn-outline-dark userinfoButton">×</button>
+                        <button type="button" class="batu">×</button>
                     </a>
                 </div>
             </div>
         </div>
+        <br>
+        <div class="row justify-content-center">
+        <h2 class="text-center" style="width: 300px;">瓶を流す</h2>
+        </div>
 
-        <h2 class="text-center mt-5">瓶を流す</h2>
         <!-- テキストエリア -->
         <!-- 瓶を流すボタン中央に配置 -->
         <?php
@@ -34,31 +41,31 @@
         echo '</div>';
 
         echo '<br><br>';
+
         echo '<div class="row justify-content-center">';
-        echo '<div class="text-center col-6">';
-        echo '<button type="button" id="nagasu" class="btn btn-outline-dark userinfoButton">瓶を流す</button>';
+        echo '<button type="submit" id="nagasu" class="btn-binwonagasu ">瓶を流す</button>';
         echo '</div>';
         echo '</div>';
+
         echo '</form>';
         ?>
 
 <!-- ダイアログ -->
         <script type="text/javascript">
             document.addEventListener("DOMContentLoaded", function() {
-                const nagasu = document.getElementById("nagasu");
-                nagasu.addEventListener("click", function() {
-                    const userInput = document.getElementById("userInput").value;
-                    const userResponse = confirm(userInput+ "\n\nこの内容でよろしいですか？");
-                    if (userResponse) {
-                        alert("瓶を流しました");
-                        window.location.href = "Binnagasu-output.php";
-                        document.getElementById("binnagasuForm").submit(); // フォームを送信
-                    } else {
-                    }
-                });
-            });
+    const nagasu = document.getElementById("nagasu");
+    nagasu.addEventListener("click", function() {
+        const userInput = document.getElementById("userInput").value;
+        const userResponse = confirm(userInput+ "\n\nこの内容でよろしいですか？");
+        if (userResponse === true) {
+            alert("瓶を流しました");
+            document.getElementById("binnagasuForm").submit(); // フォームを送信
+        } else if (userResponse === false) {
+            window.location.href = "Binnagasu-input.php";
+        }
+    });
+});  
         </script>
     </div>
 </body>
 </html>
-
