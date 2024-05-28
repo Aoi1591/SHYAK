2b2f5a7bb31482a9d1b1aeca870dfbcb9e14865e
Hiroshi <137484523+Hiroshi7700@users.noreply.github.com>
2024-05-28 10:43:39 +0900

    a

--- SHYAK/PHP/Choose your language.php
+++ SHYAK/PHP/Choose your language.php
@@ -6,14 +6,16 @@
     <link rel="stylesheet" href="../css/Chooseyourlanguage.css">
 </head>
 <body>
-    <div id="header">
-        <h1>Choose your language</h1>
-    </div>
     <div id="content">
-        <h1>この瓶でよろしいですか？</h1>
-        <input type="submit" value="はい">
-        <a href="Chooseyourlanguage.php">いいえ</a>
+        <h1>Choose your language</h1>
+    
+        <h1 style="margin:13%;">この瓶でよろしいですか？</h1>
+        <form action="top.php"method="post">
+        <input type="submit" value="はい"style="margin-right: 15%;font-size: 40px;">
+        <button type="button" class="button-link" onclick="goback()"style="margin-left: 15%;font-size: 40px;">いいえ</button>
+        </form>
     </div>
+    <script src="../js/Choose your laguage.js"></script>
 </body>
 </html>
    
