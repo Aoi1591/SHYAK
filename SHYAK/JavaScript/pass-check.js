785c6e8130aa89a5549882ca24ead805982b2e82
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-06-03 10:04:00 +0900

    送信データをPOSTに変更
    今度こそうまくいけ！！

--- SHYAK/JavaScript/pass-check.js
+++ SHYAK/JavaScript/pass-check.js
@@ -15,9 +15,29 @@ new Vue({
             const username =document.getElementById("username").value;
             const password =document.getElementById("password").value;
             const choice = document.querySelector(".LanguageChoice").value;
-            // 取得したデータをパラメータとしてURLに付加
-            var url = "signup-output.php?username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password) 
-            + "&choice=" + encodeURIComponent(choice);
+
+            // 取得したデータをオブジェクトにまとめる
+            const formData = new FormData();
+            formData.append("username", username);
+            formData.append("password",  password);
+            formData.append("choice", choice);
+
+            // fetch APIを使用し、フォームをPOSTメソッドで送信
+            fetch('signup-output.php',{
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                if(response.ok){
+                    // フォームの送信が成功したときの処理
+                    window.location.href = 'signup-output.php';
+                }else{
+                    // フォームの送信が失敗したときの処理
+                    alert('送信に失敗しました。');
+                }
+            }).catch(error => {
+                console.error('Error',error);
+                alert('フォームの送信中にエラーが発生しました');
+            });
         }
     }
 })
