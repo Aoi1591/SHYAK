f5f500b42683c9680444b610b44d5b23da533db7
koshi <2201035@s.asojuku.ac.jp>
2024-06-12 10:40:15 +0900

    瓶回収・流す仮完成

--- SHYAK/PHP/Binkaisyu-output.php
+++ SHYAK/PHP/Binkaisyu-output.php
@@ -1,72 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/Binkaisyu.css">
-</head>
-<body>
-    <br>
-    <div class="container">
-        <!-- ✘ボタン -->
-        <div class="row justify-content-end">
-            <div class="col-12 col-md-2">
-                <div class="row">
-                    <br>
-                    <a href="top.php">
-                        <button type="submit" class="batu">×</button>
-                    </a>
-                </div>
-            </div>
-        </div>
-        <br>
+<?php
+session_start(); // セッションを開始
 
-        <?php
-        echo '<div class="row justify-content-center">';
-        echo '<h2 class="text-center" style="width: 300px;">○○さんへの返事</h2>';
-        echo '</div><br>';
-        
-        echo '<form id="binkaisyuForm" action="Binkaisyu-output2.php" method="post">';
-        echo '<div class="waku">';
-        echo '<div class="row justify-content-center mt-5">';
-        echo '<div class="col-6">';
-        echo '<div class="bun"><p>"願いや祝福"</p></div><br>';
-        echo '</div>';
-        echo '</div>';
+// POSTでデータが送られてきた場合に処理
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // POSTデータからユーザー名とメッセージを取得
+    $userName = isset($_POST['user_name']) ? $_POST['user_name'] : 'Unknown';
+    $message = isset($_POST['user_message']) ? $_POST['user_message'] : '';
 
-        echo '<div class="row justify-content-center">';
-        echo '<div class="text-center col-6">';
-        echo '<textarea class="form-control" name="kaisyu" id="userInput2" rows="10" cols="40" placeholder="返事を入力してください"></textarea>';
-        echo '</div>';
-        echo '</div>';
-        echo '</div><br>';
+    // セッション変数にデータを保存
+    $_SESSION['user_name'] = $userName;
+    $_SESSION['user_message'] = $message;
 
-        echo '<div class="row justify-content-center">';
-        echo '<div class="text-center col-6">';
-        echo '<button type="submit" id="kaisyu" class="btn-binwokaisyu">瓶の返信</button>';
-        echo '</div>';
-        echo '</div>';
+    // 次のページへリダイレクト
+    header('Location: Binkaisyu-input2.php');
+    exit();
+}
 
-        echo '</form>';
-        ?>
-
-        <!-- ダイアログ -->
-        <script type="text/javascript">
-            document.addEventListener("DOMContentLoaded", function() {
-                const nagasu = document.getElementById("kaisyu");
-                nagasu.addEventListener("click", function() {
-                    const userInput = document.getElementById("userInput2").value;
-                    const userResponse = confirm(userInput + "\n\nこの内容でよろしいですか？");
-                    if (userResponse) {
-                        alert("瓶を流しました");
-                        document.getElementById("binkaisyuForm").submit(); // フォームを送信
-                    }
-                });
-            });
-        </script>
-    </div>
-</body>
-</html>
+// フォームデータがない場合、エラー処理や初期ページ表示などの処理をここに記述
+?>
---
+++ SHYAK/PHP/Binkaisyu-output2.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); // セッションを開始
+require 'connect.php'; // データベース接続
+
+// フォームからのデータがPOSTされたか確認
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['recieve_message'])) {
+    $recieveText = $_POST['recieve_message']; // ユーザーが入力した返信内容
+    $userName = $_SESSION['user_name']; // セッションからユーザー名を取得
+
+    // データベースに返信を保存するクエリ
+    $stmt = $conn->prepare("INSERT INTO Recieves (user_name, recieve_message) VALUES (?, ?)");
+    $stmt->bind_param("ss", $userName, $recieveText); // 'ss'は2つの文字列型のパラメータを意味する
+
+    // クエリの実行
+    if ($stmt->execute()) {
+        // 成功した場合、JavaScriptを用いてアラートを表示し、別のページにリダイレクトする
+        echo "<script>alert('返信に成功しました。'); window.location.href='top.php';</script>";
+    } else {
+        // 失敗した場合、エラーメッセージを表示
+        echo "<script>alert('返信に失敗しました。'); window.location.href='Binkaisyu-input2.php';</script>";
+    }
+
+    $stmt->close(); // ステートメントのクローズ
+}
+$conn->close(); // データベース接続のクローズ
+?>
