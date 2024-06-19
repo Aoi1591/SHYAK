27412e389b6808f007344a74d76a99b59523fc94
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-06-19 12:17:02 +0900

    空白削除

--- SHYAK/PHP/rejected_message.php
+++ SHYAK/PHP/rejected_message.php
@@ -1,30 +1,25 @@
 <?php session_start();?>
 <?php require 'connect.php';?>
-
 <?php
-
 //パラメータから却下するメッセージのIDを取得
 $message_id = isset($_GET['sent_id']) ? $_GET['sent_id'] : null;
-echo $message_id;
 //メッセージIDが指定されているかチェック
 if($message_id){
     try{
         //データベースに接続
         $pdo = new PDO($connect,USER,PASS);
-
         //フラグをリセット
         $sql = $pdo -> prepare("update Sents set flag = 0 WHERE sent_id = ?");
         $sql -> execute([$message_id]);
         //$stmt = $pdo -> prepare($sql);
-
         //フラグリセットが成功した場合の処理
-        header("Location : admin_message.php");
+        header("Location: admin_message.php");
         exit();
         }catch(PDOException $e){
             echo "エラー:" . $e -> getMessage();
-
         }
 }else{
     //メッセージIDが指定されていない場合の処理
     echo"却下するメッセージが指定されていません。";
 }
+?>
