b8b6742be521e65b108eee0d13653589f1c652e0
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-06-19 10:48:17 +0900

    スペルミス

--- SHYAK/PHP/admin_message.php
+++ SHYAK/PHP/admin_message.php
@@ -33,7 +33,7 @@
       $sent_id = $row['sent_id'];
       echo '<tr>';
       echo '<td>',$row['sent_id'],'</td>';
-      echo '<td>',$row['set_message'],'</td>';
+      echo '<td>',$row['sent_message'],'</td>';
       echo '<td><a href = "delete_message.php?id='.urlencode($sent_id).'">削除</a></td>';
       echo '<td><a href = "rejected_message.php?id='.urlencode($sent_id).'">却下</a></td>';
       echo '</tr>';
