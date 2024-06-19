4554f81b8500c315ab7674ec0c921075a6b830da
shion <2201376@s.asojuku.ac.jp>
2024-06-19 10:06:53 +0900

    あ

--- SHYAK/PHP/login-input.php
+++ SHYAK/PHP/login-input.php
@@ -7,28 +7,9 @@
     <link rel="stylesheet" href="../CSS/login-input.css">
 </head>
 <body>
-<?php
-        if(isset($_GET['flag']) && $_GET['flag'] == 'miss'){
-            echo '<script>alert("認証に失敗しました。");</script>';
-        }else if(isset($_GET['flag']) && $_GET['flag'] == 'fail'){
-            echo '<script>alert("エラーが発生しました。");</script>';
-        }
-    ?>
-    <!-- Background Image -->
-    <div id="back-ground"></div>
-
-    <!-- Airplane Image -->
-    <div class="airplane"></div>
-
-    <!-- Bottles Images -->
-    <div class="bottol-1"></div>
-    <div class="bottol-2"></div>
-    <div class="bottol-3"></div>
-    <div class="bottol-4"></div>
-    <div class="bottol-5"></div>
 
     <!-- Login Form Container -->
-        <a href="adminlogin_check.php" id="admin">■</a>
+
         <form action="login-output.php" method="post">
             <div id="username-all">
                 <input type="text" id="username" name="username" placeholder="USER NAME" required>
@@ -43,7 +24,7 @@
 
     <!-- Sign-Up Text and Line -->
     <div id="sign-up-container">
-        <a href="signup-input.php" id="sign-up">SIGN-UP</a>
+        <a href="../php/sign-up-input.php" id="sign-up">SIGN-UP</a>
         <div class="sign-up-line"></div>
     </div>
 </body>
