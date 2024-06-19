02b9305db01dd1b80e68fc06550ae70c1aae650a
Aoi1591 <2201039@s.asojuku.ac.jp>
2024-06-19 11:44:25 +0900

    変更

--- SHYAK/PHP/rejected_message.php
+++ SHYAK/PHP/rejected_message.php
@@ -15,7 +15,7 @@ if($message_id){
         //フラグをリセット
         $sql = $pdo -> prepare("update Sents set flag = 0 WHERE sent_id = ?");
         $sql -> execute([$message_id]);
-        $stmt = $pdo -> prepare($sql);
+        //$stmt = $pdo -> prepare($sql);
 
         //フラグリセットが成功した場合の処理
         header("Location : admin?message.php");
