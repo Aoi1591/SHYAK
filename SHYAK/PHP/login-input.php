d05b27156648a7e42654ca74177ea2b309226fd4
Aoi1591 <2401906@s.asojuku.ac.jp>
2024-07-03 09:58:22 +0900

    変更

--- SHYAK/PHP/login-input.php
+++ SHYAK/PHP/login-input.php
@@ -5,25 +5,6 @@
     <meta name=”viewport” content=”width=device-width, initial-scale=1.0″>
     <title>ログイン画面</title>
     <link rel="stylesheet" href="../CSS/login-input.css">
-    <script>
-        function adminLogin(){
-            var userInput = prompt("秘密の質問を入力してください");
-            if(userInput){
-                var form = document.createElement("form");
-                form.method = "POST";
-                form.action = "admin_login_input.php";
-
-                var input = document.createElement("input");
-                input.type = "hidden";
-                input.name = "userInput";
-                input.value = userInput;
-
-                form.appendChild(input);
-                document.body.appendChild(form);
-                form.submit();
-        }
-    }
-    </script>
 </head>
 <body>
     <?php
