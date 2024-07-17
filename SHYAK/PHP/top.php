415f4aef567d975b08df8168ad50e3f432336397
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-07-17 10:20:58 +0900

    Update top.php

--- SHYAK/PHP/top.php
+++ SHYAK/PHP/top.php
@@ -22,8 +22,10 @@ require 'menu-humburger.php';
 <!-- ログアウトの時にのみ使用-->
  <?php
       if(isset($_GET['logout'])&&$_GET['logout'] == 1){
+        $translator = new Translator();
+        $originalText = $translator->translate("ログアウトしました",$_SESSION['User']['lang']);
         echo '<script>';
-        echo 'alert("ログアウトしました")';
+        echo $originalText;
         echo '</script>';
       }
       ?>
@@ -41,7 +43,6 @@ require 'menu-humburger.php';
                 <br>
                 <?php
                     $translator = new Translator();
-                    //$originalText = $translator->translate($originalText);
                     $originalText = $translator->translate("本",$_SESSION['User']['lang']);
 
                     echo '<a href="Morattahenji.php?id=', $_SESSION['User']['id'], '"><img src="../image/hon.png" alt="', htmlspecialchars($originalText), '" class="btn-hon-image"></a>';
