18baad8ba9b760fb5904f2e3e6d07761aefe5099
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2024-07-22 09:55:04 +0900

    Chooseyourlanguageの翻訳等修正

--- SHYAK/PHP/Chooseyourlanguage.php
+++ SHYAK/PHP/Chooseyourlanguage.php
@@ -27,22 +27,64 @@
             </a>
         </div>
         <div class="jp">
-            <button type="submit" name="language" value="ja">日本語</button>
+            <button type="submit" name="language" value="ja">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "日本語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
         <div class="FR">
-            <button type="submit" name="language" value="fr">Français</button>
+            <button type="submit" name="language" value="fr">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "フランス語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
         <div class="RU">
-            <button type="submit" name="language" value="ru">Русский</button>
+            <button type="submit" name="language" value="ru">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "ロシア語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
         <div class="CN">
-            <button type="submit" name="language" value="zh-Hans">中文</button>
+            <button type="submit" name="language" value="zh-Hans">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "簡体中国語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
         <div class="EN">
-            <button type="submit" name="language" value="en">English</button>
+            <button type="submit" name="language" value="en">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "英語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
         <div class="PT">
-            <button type="submit" name="language" value="pt">Português</button>
+            <button type="submit" name="language" value="pt">
+                <?php
+                    $translator = new Translator();
+                    $originalText = "ポルトガル語";
+                    $translatedText = $translator->translate($originalText,$_SESSION['User']['lang']);
+                    echo $translatedText;
+                ?>
+            </button>
         </div>
     </form>
 </div>
