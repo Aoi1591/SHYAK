4224f8c9550ad961d368383e19c5bf5a5225be39
Aoi1591 <2401906@s.asojuku.ac.jp>
2024-07-23 23:28:22 +0900

    スクロール

--- SHYAK/PHP/Morattahenji.php
+++ SHYAK/PHP/Morattahenji.php
@@ -51,6 +51,8 @@
                             $originalText = $translator->translate($originalText,$_SESSION['User']['lang']);
                             $txtArr[$i] = $originalText;
                         }
+                        echo '<div class="scroll">';
+                        echo '<span>';
                         echo '<div class="row justify-content-center">';
                         echo '<h2 class="text-center mt-5" style="width: 300px;">';
                         echo $res['user_name'], $txtArr[0].'</h2>';
@@ -62,6 +64,8 @@
                         echo '</div>';
                         echo '</div>';
                         echo '</div>';
+                        echo'</span>';
+                        echo'</div>';
                     }
                 }
             }else{
